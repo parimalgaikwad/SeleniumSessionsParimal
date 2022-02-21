a14a440fd21aded127e2c3f1f7bddcd09b3b3d2b
Parimal Gaikwad <95343304+parimal-gaikwad@users.noreply.github.com>
2022-02-21 18:14:01 +0530

    java files and task

---
+++ src/main/java/JavaScriptExecutors.java
@@ -0,0 +1,45 @@
+
+package session;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class JavaScriptExecutors {
+	WebDriver driver;
+	
+	@Test
+	public void test() {
+	
+	WebDriverManager.chromedriver().setup();
+	driver = new ChromeDriver();
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	driver.navigate().to("https://chercher.tech/practice/explicit-wait-sample-selenium-webdriver");
+	driver.manage().window().maximize();
+	
+	JavascriptExecutor js = (JavascriptExecutor) driver;
+
+	try {
+		Thread.sleep(10000);
+	} catch (InterruptedException e) {
+		e.printStackTrace();
+	}
+
+	js.executeScript("window.scrollBy(0,500)", "");   // to scroll down
+
+	try {
+		Thread.sleep(10000);
+	} catch (InterruptedException e) {
+		e.printStackTrace();
+	}
+	js.executeScript("window.scrollBy(0,-500)", "");   // to scroll up
+
+	
+
+	}
+}
